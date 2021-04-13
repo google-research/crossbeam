a707742f8229894f2ebeb119a5206918c88ab505
Hanjun Dai <hadai@google.com>
2021-04-13 11:55:32 -0700

    address all comments left in pull/2

--- crossbeam/algorithm/beam_search.py
+++ crossbeam/algorithm/beam_search.py
@@ -4,6 +4,7 @@ import flax
 from flax import linen as nn
 import functools
 import math
+from crossbeam.model.util import ceil_power_of_2
 
 
 @functools.partial(jax.jit, static_argnums=[0, 1, 6])
@@ -14,11 +15,18 @@ def beam_search(arity, k, choice_embed, choice_mask, init_embed, params, score_m
     k: beam_size;
     choice_embed: array of size [N, embed_dim], embedding of N choices;
     choice_mask: vector of size N, mark valid (1) or invalid choice(0)
-    init_state: embedding of initial state
-    state_update_func: a callable that takes (state, update_embed) as input,
-      and output new state; here state has the same shape as init_state,
-      while update_embed is a single row of choice_embed.
-      Note that parallel update for beam_size > 1 is done with jax.vmap;
+    init_embed: embedding of initial state
+    params: FrozenDict of nn parameters, used by score_model
+    score_model: an nn module that implements fn_init_state, fn_step_state, fn_step_score
+      fn_init_state: initialize score_model state with init_embed
+      fn_step_state: a function takes (state, update_embed) as input,
+        and output new state; Note that parallel update for beam_size > 1 is done with jax.vmap;
+        so fn_step_state only takes care of single state update
+      fn_step_score: a function takes (N-state, M-choices) and return a score matrix of N x M
+
+  Returns:
+    arg_choices: jax int32 array of size k x arity, the indices of selected args
+    prefix_scores: scores for each arg-list
   """
   state_update_func = score_model.fn_step_state(params)
   vmap_state_func = jax.vmap(state_update_func)
@@ -52,26 +60,15 @@ def beam_search(arity, k, choice_embed, choice_mask, init_embed, params, score_m
   return arg_choices, prefix_scores
 
 
-def ceil_power_of_2(x):
-  return 1 if x == 0 else 2 ** (x - 1).bit_length()
-
-
 def padded_beam_search(arity, k, choice_embed, init_embed, params, score_model):
-  """
-  Args:
-    arity: int, arity of current op;
-    k: beam_size;
-    choice_embed: array of size [N, embed_dim], embedding of N choices;
-    init_state: pytree of vectors with length state_dim;
-    state_update_func: a callable that takes (state, update_embed) as input,
-      and output new state; here state has the same shape as init_state,
-      while update_embed is a single row of choice_embed.
-      Note that parallel update for beam_size > 1 is done with jax.vmap;
-    score_func: a callable that takes (M_states, choice_embed) as input,
-      and output MxN score matrix for all the choices. Here M_states is a pytree
-      of matrices with shape M x state_dim;
+  """This one pad the choices into size of power-of-2.
+
+  See beam_search for the args.
   """
   num_choices = choice_embed.shape[0]
+  #TODO: Depending on how expensive each compilation is,
+  # we might not need buckets for small sizes,
+  # e.g., any num_choices <= 128 leads to padded_shape=128
   padded_shape = ceil_power_of_2(num_choices)
   choice_mask = jnp.pad(jnp.ones(num_choices), [(0, padded_shape - num_choices)])
   choice_embed = jnp.pad(choice_embed, [(0, padded_shape - num_choices), (0, 0)])
