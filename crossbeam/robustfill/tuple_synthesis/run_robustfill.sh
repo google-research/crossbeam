5f6df6c510e49d4ade769fd7d61865f1fcae6378
Hanjun Dai <hadai@google.com>
2021-04-26 20:38:11 +0000

    minor

--- crossbeam/robustfill/tuple_synthesis/run_robustfill.sh
+++ crossbeam/robustfill/tuple_synthesis/run_robustfill.sh
@@ -6,8 +6,9 @@ data_folder=$HOME/data/crossbeam/tuple_synthesis
 
 embed=128
 bsize=512
+rnn_layers=3
 beam_size=4
-save_dir=$HOME/results/crossbeam/robustfill/tuple_synthesis/e-$embed-b-$bsize
+save_dir=$HOME/results/crossbeam/robustfill/tuple_synthesis/e-$embed-b-$bsize-r-$rnn_layers
 
 if [ ! -e $save_dir ];
 then
@@ -16,6 +17,7 @@ fi
 
 python main_robustfill.py \
     --gpu 0 \
+    --decoder_rnn_layers $rnn_layers \
     --data_folder $data_folder \
     --save_dir $save_dir \
     --lr 1e-3 \
@@ -25,4 +27,3 @@ python main_robustfill.py \
     --train_steps 1000000 \
     --batch_size $bsize \
     --n_para_dataload 8 \
-    --load_model model-best-valid.ckpt \
