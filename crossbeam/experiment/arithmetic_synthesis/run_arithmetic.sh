f047ed31fe1fc6e649d8626efba312bc96aff6d2
Hanjun Dai <hadai@google.com>
2021-04-27 17:56:45 +0000

    sync script

--- crossbeam/experiment/arithmetic_synthesis/run_arithmetic.sh
+++ crossbeam/experiment/arithmetic_synthesis/run_arithmetic.sh
@@ -3,8 +3,10 @@
 
 data_folder=$HOME/data/crossbeam/arithmetic_synthesis
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 python main_arithmetic.py \
     --data_folder $data_folder \
-    --gpu -1 \
+    --eval_every 10000 \
+    --train_steps 1000000 \
+    --gpu 0 \
--- crossbeam/experiment/tuple_synthesis/run_main.sh
+++ crossbeam/experiment/tuple_synthesis/run_main.sh
@@ -7,4 +7,6 @@ export CUDA_VISIBLE_DEVICES=0
 
 python main_tuple.py \
     --data_folder $data_folder \
-    --gpu -1 \
+    --eval_every 10000 \
+    --train_steps 1000000 \
+    --gpu 0 \
