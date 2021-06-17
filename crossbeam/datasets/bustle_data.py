fe53ac15bea16b2a751454b37a7196f02d3922b7
Kensen Shi <6374262+kensens@users.noreply.github.com>
2021-06-17 07:24:29 +0000

    initial constant extractor, needs testing

--- crossbeam/datasets/bustle_data.py
+++ crossbeam/datasets/bustle_data.py
@@ -1,3 +1,4 @@
+import itertools
 import random
 
 
@@ -23,9 +24,63 @@ def bustle_input_generator():
 
 # TODO(kshi): Reimplement GenerateData::getRandomExamples
 
+ALWAYS_USED_CONSTANTS = ['', 0, 1, 2, 3, 99]
+COMMON_CONSTANTS = [
+    ' ', ',', '.', '!', '?', '(', ')', '[', ']', '<', '>', '{', '}', '-', '+',
+    '_', '/', '$', '#', ':', ';', '@', '%', '0']
+
+
+def compute_lcs(str1, str2):
+  len1 = len(str1)
+  len2 = len(str2)
+  # dp[i][j] = length of longest common substring of str1[0..i] and str2[0..j]
+  dp = [[0] * len1 for _ in range(len2)]
+
+  bestSubstring = ''
+  bestLength = 0
+  for i in range(1, len1 + 1):
+    for j in range(1, len2 + 1):
+      if str1[i-1] == str2[j-1]:
+        dp[i][j] = dp[i-1][j-1] + 1
+        if dp[i][j] > bestLength:
+          bestLength = dp[i][j]
+          bestSubstring = str1[i - bestLength : i]
+  return bestSubstring
+
 
 def bustle_constants_extractor(task):
-  # TODO(kshi): Reimplement ConstantExtraction::extractConstants
-  del task
-  return ['', ' ', 0, 1, 2, 99]
+  """Extracts constants from the input-output pairs.
 
+  Extracted constants include:
+   1. The longest common susbtring between any pair of output strings, or any
+      pair of input strings from the same column, if it has >= 2 characters.
+   2. Any string constant from the provided list of common string constants, if
+      it is a substring of any input or output.
+  """
+  # TODO(kshi): Reimplement ConstantExtraction::extractConstants
+
+  extracted_constants = []
+  extracted_constants_set = set()
+
+  string_columns = list(task.inputs_dict.values())
+  if task.outputs:
+    string_columns.append(task.outputs)
+
+  # Criteria 1: longest common substrings.
+  lcs_pairs = sum(itertools.combinations(column)
+                  for column in string_columns)
+  for x, y in lcs_pairs:
+    lcs = compute_lcs(x, y)
+    if len(lcs) >= 2:
+      extracted_constants_set.add(lcs)
+
+  # Criteria 2: common string constants.
+  for common in COMMON_CONSTANTS:
+    if any(common in s for s in column for column in string_columns):
+      extracted_constants_set.add(common)
+
+  # Sort the constants by length and then alphabetically.
+  extracted_constants = list(extracted_constants_set)
+  collections.sort(extracted_constants_set, key=lambda s: (len(s), s))
+
+  return ALWAYS_USED_CONSTANTS + extracted_constants
