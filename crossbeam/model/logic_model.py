5ba0d1b0b93989c6f2c67373071636101f6aa632
ellisk42@gmail.com <ellisk42@gmail.com>
2021-08-06 15:05:08 -0400

    Logic model now uses great transformer

--- crossbeam/model/logic_model.py
+++ crossbeam/model/logic_model.py
@@ -4,11 +4,15 @@ import torch.nn as nn
 
 from crossbeam.model.op_arg import LSTMArgSelector
 from crossbeam.model.op_init import OpPoolingState
+from crossbeam.model.great import Great
 
 
 class LogicModel(nn.Module):
   def __init__(self, args, operations, max_entities=10, hidden=256):
     super(LogicModel, self).__init__()
+
+    self.max_entities = max_entities
+    
 
     self.arg = LSTMArgSelector(hidden_size=args.embed_dim,
                                mlp_sizes=[256, 1],
@@ -16,6 +20,28 @@ class LogicModel(nn.Module):
                                step_score_normalize=args.score_normed)
     self.init = OpPoolingState(ops=tuple(operations), state_dim=args.embed_dim, pool_method='mean')
 
+    self.entity_project = nn.Embedding(max_entities, args.embed_dim)
+    
+    # relations:
+    # unary both no
+    # unary first yes
+    # unary second yes    
+    # unary both yes    
+    # binary no
+    # binary ->
+    # binary <-
+    # binary <->
+    # also applies for spec (2x)
+    self.relation_project = nn.Embedding(8+8, args.embed_dim)
+
+    self.great = Great(d_model=args.embed_dim,
+                       dim_feedforward=args.embed_dim*4,
+                       layers=4,
+                       batch_first=True)
+
+    self.final_projection = nn.Linear(max_entities*args.embed_dim,args.embed_dim)
+
+    # deprecated
     self.embed_spec,self.embed_value,self.embed_input = \
                       [ nn.Sequential(nn.Linear(max_entities*max_entities + 1, hidden),
                                      nn.ReLU(),
@@ -33,6 +59,40 @@ class LogicModel(nn.Module):
     else:
       assert False, "only handle relations with 1/2 indices"
 
+  def features_of_relation(self, relations, is_specification):
+    x = self.entity_project(torch.arange(self.max_entities).long())
+    x = x.unsqueeze(0).repeat(len(relations),1,1)
+
+    d = {(False,False): 0,
+         (False,True): 1,
+         (True,False): 2,
+         (True,True): 3}
+
+    def o(a):
+      nonlocal is_specification
+      h = {1:0,2:4}[len(a.shape)]
+      if is_specification:
+        return h+8
+      return h
+
+    def I(matrix,a,b):
+      if len(matrix.shape) == 2:
+        return matrix[a,b]
+      if len(matrix.shape) == 1:
+        return matrix[a]
+      assert False
+
+    r = [ [ [ o(m) + d[I(m,i,j), I(m,j,i)]
+              for j in range(self.max_entities)]
+            for i in range(self.max_entities)]
+          for m in relations ]
+    
+    r = self.relation_project(torch.tensor(r).long().to(x.device))
+
+    output = self.great(x,r).view(len(relations),-1)
+    output = self.final_projection(output)
+    return output
+
   def io(self, input_dictionary, outputs, device):
     """input_dictionary/outputs: list of length batch_size, batching over task
     Each element of outputs is the output for a particular I/O example
@@ -42,10 +102,19 @@ class LogicModel(nn.Module):
     assert len(input_dictionary) == 1
     assert len(outputs) == 1
     assert len(outputs[0]) == 1
-    
+
     input_dictionary = input_dictionary[0]
     outputs = outputs[0][0]
+    specification = self.features_of_relation([outputs], True)
 
+    
+    values = self.val(list(input_dictionary.values()),
+                      specification.device)
+    
+    values = values.max(0).values.unsqueeze(0)
+
+    return torch.cat((specification,values),-1)
+    # deprecated
     output_embedding = self.embed_spec(torch.tensor(LogicModel.serialize_relation(outputs)).float().to(device))
     
     
@@ -57,6 +126,7 @@ class LogicModel(nn.Module):
     """all_values: list of values. each value is represented by its instantiation on each I/O example
     so if you have three I/O examples and ten values then you will have 10x3 matrix as input
     returns as output [number_of_values,embedding_size]"""
-
-    x = torch.tensor([[LogicModel.serialize_relation(r) for r in v] for v in all_values]).float().to(device)
-    return self.embed_value(x).max(-2).values
+    
+    all_values = [v[0] for v in all_values]
+    return self.features_of_relation(all_values, False).to(device)
+    
