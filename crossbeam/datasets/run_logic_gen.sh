2b639619e8438bb19cce3b064cde09808ff470d8
ellisk42@gmail.com <ellisk42@gmail.com>
2021-09-14 20:46:36 -0400

    miscellaneous clean up and getting script ready

--- crossbeam/datasets/run_logic_gen.sh
+++ crossbeam/datasets/run_logic_gen.sh
@@ -12,7 +12,7 @@ fi
 seed=10
 eval_file=$data_dir/test-tasks.pkl
 max_size=15
-
+let time_out=60*60 # an hour of enumeration
 
 python data_gen.py \
     --domain=logic  --num_searches 1\
@@ -38,7 +38,11 @@ then
     mkdir -p $data_dir
 fi
 
-for fn in $data_dir/test-tasks.pkl $data_dir/valid-tasks.pkl,2r $data_dir/train-tasks.pkl; do
-	  echo "processing", $fn
-	  python bottom_up_data_generation.py --domain=logic  --num_searches 1 --max_num_examples=1 --min_num_examples=1 --max_num_inputs=5 --min_num_inputs=4 --output_file $fn --max_task_weight $max_size
+NT=1000000 # number of training tasks
+for fn in $data_dir/train-tasks.pkl $data_dir/test-tasks.pkl $data_dir/valid-tasks.pkl; do
+    echo "producing output in" $fn
+    
+    python bottom_up_data_generation.py --domain=logic  --num_searches 1 --max_num_examples=1 --min_num_examples=1 --max_num_inputs=4 --min_num_inputs=4 --output_file $fn --max_task_weight $max_size --data_gen_timeout=$time_out  --num_tasks=$NT
+
+    NT=1000 # number of testing and validation tasks
 done
