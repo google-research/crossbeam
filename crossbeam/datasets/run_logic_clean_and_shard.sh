a25309f572db0f16f44e40d2f64e82ba2dc32dde
Kensen Shi <6374262+kensens@users.noreply.github.com>
2022-03-08 08:56:35 +0000

    logic data

--- crossbeam/datasets/run_logic_clean_and_shard.sh
+++ crossbeam/datasets/run_logic_clean_and_shard.sh
@@ -22,9 +22,9 @@ then
 	mkdir -p $output_root
 fi
 
-#python clean_logic.py \
-#  $data_root/train-tasks.pkl \
-#  $output_root/train-tasks.pkl \
+python clean_logic.py \
+  $data_root/train-tasks.pkl \
+  $output_root/train-tasks.pkl \
 
 python make_shards.py \
   $output_root
