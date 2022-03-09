b8389c87e0c2237d09fe5fe1840d743ee31b3c08
Kensen Shi <6374262+kensens@users.noreply.github.com>
2022-03-09 00:19:28 +0000

    license

--- iclr2022/plot_expressions.py
+++ iclr2022/plot_expressions.py
@@ -1,7 +1,20 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Plots the number of programs synthesized by the elapsed time.
 
 python3 plot_expressions.py --output_file=string_manipulation_exps_plot.png
-
 """
 
 import ast
