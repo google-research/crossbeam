0d67bf198d04feb881257dcf2400fc7f9671506c
Hanjun Dai <daihanjun@gmail.com>
2022-12-01 06:22:05 +0000

    fix memory issue on multiple gpus

--- crossbeam/experiment/deepcoder/run_deepcoder.sh
+++ crossbeam/experiment/deepcoder/run_deepcoder.sh
@@ -22,7 +22,7 @@ then
     mkdir -p $save_dir
 fi
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 python3 -m crossbeam.experiment.run_crossbeam \
     --config="configs/${config_name?}.py" \
